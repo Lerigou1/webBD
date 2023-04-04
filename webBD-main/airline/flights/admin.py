396d8b11fc69b974b45604925c40a4849b9f9ff2
Lerigou <115087583+Lerigou1@users.noreply.github.com>
2023-04-04 16:26:49 -0300

    Delete webBD-main/airline directory

--- webBD-main/airline/flights/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Flight, Airport, Passenger, FlightAdmin
-
-admin.site.register(Flight, FlightAdmin)
-admin.site.register(Airport)
-admin.site.register(Passenger)
